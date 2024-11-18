941ea80a5b6ff3c04b9a6993e8759cef92ddb5a3
RubyD <143292762+ohnoruu@users.noreply.github.com>
2024-11-18 19:20:14 +0000

    wip

--- Main.java
+++ Main.java
@@ -4,22 +4,39 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    printNTimes("code",3);
+    System.out.println();
+    printNums();
+    uprightNumberTriangle(5);
   }
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int i = 0; i<word.length();i++){
+      for (int numTimes = 0; numTimes<N;numTimes++){
+        String L = word.substring(i,i+1);
+        System.out.print(L);
+      }
+    }
   }
 
   public static void printNums()
   {
-    // code solution here
+    for (int numDescend = 10; numDescend > 0; numDescend--){ //numbers descending
+      for (int repeat = numDescend; repeat > 0; repeat--){ //times number is repeated
+        System.out.print(numDescend + " ");
+      }
+      System.out.println();
+    }
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    for (int numAscend = 1; numAscend<=N; numAscend++){ //numbers increasing up to N
+      for (int printUpTo = numAscend; printUpTo<=N; printUpTo++){
+        System.out.print(printUpTo);
+      }
+    }
   }
 
   public static void starTree()
